38a71b62d2343d66a21d12b50c37df9ad3f964e8
Alessandro Terracciano <terracciano41@gmail.com>
2022-01-28 10:05:26 +0100

    jsnack3 completato

---
+++ jsnack3/main.js
@@ -0,0 +1,30 @@
+/* Calcola la somma e la media dei primi 10 numeri. */
+
+let somma = 0;
+
+let i=0;
+
+let stop = false;
+
+while ( i < 10  ) {
+
+    let numero = ParseInt(prompt(' Inserisci un numero'));
+
+    if (isNaN(numero)) {
+
+        alert('numero non valido, inserisco 0')
+
+        numero = 0;      //se utente inserisce un NaN inserisco lo 0
+    }
+
+    somma += somma;
+
+    i++;
+
+
+}
+
+/*stampo la somma */
+
+alert('la somma vale : ' + somma);
+
